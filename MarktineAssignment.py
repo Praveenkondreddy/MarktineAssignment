da0ea286a3e2d7ad0f989cb49d9aa69aab1e9d54
Praveenkondreddy <88034196+Praveenkondreddy@users.noreply.github.com>
2021-07-28 08:04:59 +0530

    Add files via upload

---
+++ MarktineAssignment.py
@@ -0,0 +1,15 @@
+a=input().split(",")
+print(a)
+n=[]
+for i in a:
+    l=[]
+    for j in a:
+        if sorted(i)==sorted(j):
+            l.append(j)
+    n.append(sorted(l))
+unique_list=[]    
+for x in n:
+        # check if exists in unique_list or not
+        if x not in unique_list:
+            unique_list.append(x)
+print(unique_list)
