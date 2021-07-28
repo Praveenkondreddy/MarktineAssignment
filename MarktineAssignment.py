1fac9301bc2a4b592ccfb3672403ce2c03382968
Praveenkondreddy <88034196+Praveenkondreddy@users.noreply.github.com>
2021-07-28 08:26:50 +0530

    Update MarktineAssignment.py

--- MarktineAssignment.py
+++ MarktineAssignment.py
@@ -8,7 +8,6 @@ for i in a:
     n.append(sorted(l))
 unique_list=[]    
 for x in n:
-        # check if exists in unique_list or not
         if x not in unique_list:
             unique_list.append(x)
 print(unique_list)
